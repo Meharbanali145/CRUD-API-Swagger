3b4153e47a87c9abbc6c79cb9f8b97a853e8bab2
Meharban Ali <alimeharban760@gmail.com>
2026-07-18 21:17:33 +0500

    Stage 1: root and health endpoint

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello Server");
 });
 
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"],
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    timestamp: new Date(),
+  });
+});
+
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });

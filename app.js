bdec0e74cb8c032a1285c01e28bc72a13772f872
Meharban Ali <alimeharban760@gmail.com>
2026-07-18 21:21:26 +0500

    Stage 2: read endpoints with 404

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
+let tasks = [
+  {
+    id: 1,
+    title: "task1",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "task02",
+    done: false,
+  },
+  {
+    id: 3,
+    title: "task03",
+    done: false,
+  },
+];
+
 app.get("/", (req, res) => {
     res.status(200).send("Hello Server");
 });
@@ -21,6 +39,26 @@ app.get("/health", (req, res) => {
     status: "OK",
     timestamp: new Date(),
   });
+});
+
+app.get("/tasks", (req, res) => {
+  return res.status(200).json({
+    tasks,
+  });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+
+  const task = tasks.find((t) => t.id === taskId);
+
+  if (!task) {
+    return res.status(404).json({
+      error: `Task ${req.params.id} not found`,
+    });
+  }
+
+  return res.status(200).json({ task });
 });
 
 app.listen(3000, () => {

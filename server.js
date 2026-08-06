147720a4f649efeb2f03756ea6e54d09892a2258
Meharban Ali <alimeharban760@gmail.com>
2026-08-06 16:28:07 +0500

    Stage 1: singup and login routes working

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const { initDb } = require('./src/db/db');
 const app = require('./src/app');
-const supabase = require('./supabaseClient');
 
 async function start() {
   await initDb();
-  app.listen(3000, () => {
-    console.log("Server is listening on port 3000 and connected to Supabase");
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is listening and connected to Supabase");
   });
 }
 
--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routes = require('./routes/task.route');
+const authRoutes = require('./routes/auth.route');
 
 const app = express();
 app.use(express.json());
 app.use('/api', routes);
+app.use('/auth', authRoutes);
 
 module.exports = app;
